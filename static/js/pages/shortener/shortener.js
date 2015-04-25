4ae63e3d7f0c72231ead35a649fa857d8025c774
William DeFreese <alexdefreese@gmail.com>
2015-04-25 14:23:40 -0400

    Add shortener.js
    Added code to parse browser data, find plugins, and perform redirections.

--- static/js/pages/shortener/shortener.js
+++ static/js/pages/shortener/shortener.js
@@ -5,8 +5,69 @@ function websocket_url(path) {
     return scheme + window.location.host + path;
 }
 
+function redirect_browser(update) {
+    window.location.replace(update["redirect_url"])
+}
+
+function parse_plugin(plugin) {
+    var plugin_data = {};
+    plugin_data['name'] = plugin['name'];
+    plugin_data['description'] = plugin['description'];
+    plugin_data['version'] = plugin['version'];
+    return plugin_data;
+}
+
+function get_plugins() {
+    var plugin_list = [];
+    var plugin;
+    for (plugin in navigator.plugins) {
+        plugin_list.push(parse_plugin(navigator.plugins[plugin]));
+    }
+
+    return plugin_list;
+}
+
+function get_browser_data() {
+    var browser_data = {};
+    browser_data['name'] = navigator.appName;
+    browser_data['version'] = navigator.appVersion;
+    browser_data['codename'] = navigator.appCodeName;
+
+    browser_data['platform'] = navigator.platform;
+    browser_data['user_agent'] = navigator.userAgent;
+    browser_data['oscpu'] = navigator.oscpu;
+
+    return browser_data;
+}
+
+function fingerprint() {
+    var analysis_message = {
+        'opcode': 'analyze'
+    };
+
+    analysis_message['browser'] = get_browser_data();
+
+    analysis_message['plugins'] = get_plugins();
+
+    monitor_socket.send(JSON.stringify(analysis_message));
+}
+
 $(document).ready(function() {
 
-	var url = websocket_url("/connect/analysis/")
+	var url = websocket_url("/connect/analysis?uuid=" + $("#connect-uuid").data("uuid"));
+    monitor_socket = new WebSocket(url);
+
+    monitor_socket.onopen = function() {
+        // TODO: Add onopen code here
+    };
+
+    monitor_socket.onmessage = function (evt) {
+        var update = $.parseJSON(evt.data);
+        if (update['opcode'] == 'redirect') {
+            redirect_browser(update);
+        }
+    };
+
+    fingerprint()
 
 });
