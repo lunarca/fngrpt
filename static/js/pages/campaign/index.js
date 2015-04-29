9b084c807aede4ca75eb80109877596667ac6a07
William DeFreese <alexdefreese@gmail.com>
2015-04-29 19:09:42 -0400

    Add link to view campaign, add view page

--- static/js/pages/campaign/index.js
+++ static/js/pages/campaign/index.js
@@ -1,3 +1,9 @@
 /**
  * Created by lunarca on 4/25/15.
  */
+
+$(document).ready(function() {
+    $("a[id^=view-campaign]").click(function() {
+        window.location = "/campaigns/manage/" + $(this).data('uuid');
+    });
+});
