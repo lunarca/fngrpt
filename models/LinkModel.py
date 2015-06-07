fc5870f51565a240229a2b93c3e79d018261ca98
William DeFreese <alexdefreese@gmail.com>
2015-06-07 10:41:19 -0400

    Fix foreign keys to Campaign models

--- models/LinkModel.py
+++ models/LinkModel.py
@@ -30,7 +30,7 @@ class Link(DatabaseObject):
 
     display_id = Column(String(8), unique=True, default=generate_dispid)
 
-    campaign_id = Column(Integer, ForeignKey('campaigns.id'),
+    campaign_id = Column(Integer, ForeignKey('campaign.id'),
                          nullable=False,)
 
     target_id = Column(Integer, ForeignKey('target.id'),
